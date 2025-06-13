a2fb75a52a82b8c091e905358813fa690603cad9
danyagithb <danil2019sinegub@gmail.com>
2025-06-13 21:04:00 +0200

    homework 2 / -task 1

--- src/components/todos-component/Todos.tsx
+++ src/components/todos-component/Todos.tsx
@@ -1,7 +1,13 @@
 import './Todos.css'
+import {useEffect, useState} from "react";
+import type {TodoModel} from "../../models/todo-model.ts";
 
 export const Todos = () => {
+    const [todos, setTodos] = useState<TodoModel[]>()
+    useEffect(() => {
+        // loadTodos.then()
+    }, [])
     return (
-        <></>
+        <div></div>
     );
 };
