32b50ebfd47512455023c1153468ead10b9561dc
danyagithb <danil2019sinegub@gmail.com>
2025-09-14 20:14:04 +0200

    homework 3 / task 1

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+
+const Menu = () => {
+    return (
+        <div>
+            <ul>
+                <li><Link to={''}>Home</Link></li>
+                <li><Link to={'users'}>Users</Link></li>
+                <li><Link to={'posts'}>Posts</Link></li>
+                <li><Link to={'comments'}>Comments</Link></li>
+                <li><Link to={'products'}>Products</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Menu;
