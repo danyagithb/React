029151c391a2179dae880ead6e9adb504fbaba02
danyagithb <danil2019sinegub@gmail.com>
2025-09-23 15:53:57 +0200

    homework 3 / task 6 completed

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,12 +1,8 @@
 import UsersComponent from "../components/users/UsersComponent.tsx";
-import {Outlet} from "react-router-dom";
 
 const UsersPage = () => {
     return (
         <div>
-            <hr/>
-            <Outlet/>
-            <hr/>
             <UsersComponent/>
         </div>
     );
