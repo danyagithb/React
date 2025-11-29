09d7ebc3164afffe3eda363dd3f6a2719dcc4788
danyagithb <danil2019sinegub@gmail.com>
2025-11-29 13:44:39 +0100

    lessons 8 - начало. сделал роутеры и стор

---
+++ src/redux/store.ts
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+        userStoreSlice: userSlice.reducer,
+        postStoreSlice: postSlice.reducer,
+        commentStoreSlice: commentSlice.reducer,
+    }
+});
